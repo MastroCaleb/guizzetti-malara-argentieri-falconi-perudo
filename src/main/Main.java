d7dd5e910039955f9cba08494bf521483c68a63d
Guizzetti <zyyper4509@gmail.com>
2024-01-17 10:52:01 +0100

    Open console from jar

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.awt.*;
+import java.io.Console;
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import network.client.Client;
@@ -8,8 +12,30 @@ import utils.input.In;
 
 public class Main {
     private static final Logger LOGGER = Logger.getLogger("Main");
+    static boolean console = false; //Make true only for jar builds
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+        if(console){
+            openConsole();
+        }
+        else{
+            startPerudo();
+        }
+    }
+
+    private static void openConsole() throws IOException, InterruptedException, URISyntaxException {
+        Console console = System.console();
+        if(console == null && !GraphicsEnvironment.isHeadless()){
+            String filename = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            Runtime.getRuntime().exec(getConsoleCommand(filename));
+        }
+        else{
+            startPerudo();
+            System.out.println("Program has ended, please type 'exit' to close the console");
+        }
+    }
+
+    private static void startPerudo(){
         try {
             while(true) {
                 System.out.println("[---START MENU---]");
@@ -45,6 +71,17 @@ public class Main {
         }
         catch (Exception var4) {
             LOGGER.log(Level.SEVERE, "IOException encountered. Not the correct input.");
+        }
+    }
+
+    private static String[] getConsoleCommand(String filename){
+        String os = System.getProperty("os.name");
+
+        if(os.contains("Windows")){
+            return new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + filename + "\""};
+        }
+        else{
+            return new String[]{"sh","-c","xdg-open","sh","--init-file","java -jar \"" + filename + "\""};
         }
     }
 }
